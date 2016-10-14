acfe710e00a1e26083284a555d84f1faa03e528a
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-14 14:59:59 +0200

    Add ProgramaA05 (Ejercicio02UD2)

---
+++ src/dam/programacion/juanminm/Ejercicio02UD2/ProgramaA05.java
@@ -0,0 +1,40 @@
+/*
+ * 5. Escribe un programa que permita introducir una calificación (entre cero y
+ * diez) e imprima su equivalente alfabético, según la siguiente tabla. 
+ *
+ *    De 3 ------------------------> M.D. 
+ *    Desde 3 y menor que 5 -------> INS. 
+ *    Desde 5 y menor que 6 -------> SUF. 
+ *    Desde 6 y menor que 7 -------> BIEN 
+ *    Desde 7 y menor que 9 -------> NOT. 
+ *    Desde 9 hasta 10 ------------> SOB.
+ */
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+import java.util.Scanner;
+
+public class Ejercicio02UD2_A05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        float nota;
+        
+        System.out.print("Introduzca la nota: ");
+        nota = scan.nextFloat();
+        if (nota < 4) {
+            System.out.println("M.D.");
+        } else if (nota < 5) {
+            System.out.println("INS.");
+        } else if (nota < 6) {
+            System.out.println("SUF.");
+        } else if (nota < 7) {
+            System.out.println("BIEN");
+        } else if (nota < 9) {
+            System.out.println("NOT.");
+        } else {
+            System.out.println("SOB.");
+        }
+    }
+}
