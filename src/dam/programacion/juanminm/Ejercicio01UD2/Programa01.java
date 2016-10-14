a164bf0b8723926d2161e3caa50196363d193e53
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-14 14:59:42 +0200

    Add Programa01 (Ejercicio01UD2)

---
+++ src/dam/programacion/juanminm/Ejercicio01UD2/Programa01.java
@@ -0,0 +1,14 @@
+/*
+ * Ejercicio 1.
+ * 
+ * Escribir un programa que escriba en la pantalla tu nombre completo en
+ * una línea y en la línea siguiente tu fecha de nacimiento.
+ */
+
+
+public class EjercicioU2_01 {
+    public static void main(String[] args) {
+        System.out.println("Juan Miguel Navarro Martínez.");
+        System.out.println("1993/07/16");
+    }
+}
