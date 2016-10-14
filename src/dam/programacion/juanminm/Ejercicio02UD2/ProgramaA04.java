e7f0f57aea3b702d089da28adf35ed82a16ca986
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-14 14:59:58 +0200

    Add ProgramaA04 (Ejercicio02UD2)

---
+++ src/dam/programacion/juanminm/Ejercicio02UD2/ProgramaA04.java
@@ -0,0 +1,28 @@
+/*
+ * 4. Escribe un programa que solicite  dos números reales y los escriba
+ * ordenados de menor a mayor
+ */
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+import java.util.Scanner;
+
+public class Ejercicio02UD2_A04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double numero1, numero2;
+        
+        System.out.print("Introduzca el primer numero: ");
+        numero1 = scan.nextDouble();
+        System.out.print("Introduzca el segundo numero: ");
+        numero2 = scan.nextDouble();
+        
+        if (numero1 >= numero2) {
+            System.out.println(numero1 + ", " + numero2);
+        } else {
+            System.out.println(numero2 + ", " + numero1);
+        }
+    }
+}
