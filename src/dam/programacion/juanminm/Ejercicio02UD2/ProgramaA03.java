c67473bcb64d952ebad4b3911a29b65b59e22e35
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-14 14:59:57 +0200

    Add ProgramaA03 (Ejercicio02UD2)

---
+++ src/dam/programacion/juanminm/Ejercicio02UD2/ProgramaA03.java
@@ -0,0 +1,27 @@
+/*
+ * 3. Escriba un programa en el que se solicite un número entero al usuario y
+ * el programa escribirá un mensaje por pantalla que indique si se trata de un
+ * número par o de un número impar
+ */
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+import java.util.Scanner;
+
+public class Ejercicio02UD2_A03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numero;
+        
+        System.out.print("Introduzca un número: ");
+        numero = scan.nextInt();
+        
+        if ( numero % 2 == 0) {
+            System.out.printf("El número %d es par.\n", numero);
+        } else {
+            System.out.printf("El número %d es impar.\n", numero);
+        }
+    }
+}
