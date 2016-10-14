0a2ffce062a660f0a8c0bc29158eaa2d5e7fbd16
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-14 15:00:06 +0200

    Add ProgramaB04 (Ejercicio02UD2)

---
+++ src/dam/programacion/juanminm/Ejercicio02UD2/ProgramaB04.java
@@ -0,0 +1,43 @@
+/*
+ * 4. Introducir A y B que sea mayor que A. Visualizar los números de A hasta B
+ * e indicar cuantos hay que sean pares
+ */
+package dam.programacion.juanminm.Ejercicio02UD2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juam Miguel Navarro Martínez
+ */
+public class ProgramaB04 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a, b, pares = 0;
+        
+        System.out.print("Introduzca primer número: ");
+        a = s.nextInt();
+        
+        do {
+            System.out.print("Introduzca segundo número (debe ser mayor que el "
+                    + "primero: ");
+            b = s.nextInt();
+        } while (a >= b);
+        
+        System.out.println("Los  numeros del " + a + " al " + b + " son:");
+        for (int i = a; i <= b; i++) {
+            if (i < b) {
+                System.out.print(i + ", ");
+            } else {
+                System.out.println(i);
+            }
+            
+            if (i % 2 == 0) {
+                pares++;
+            }
+        }
+        
+        System.out.println("\nHay " + pares + " pares entre el " + a + " y el "
+                + b);
+    }
+}
