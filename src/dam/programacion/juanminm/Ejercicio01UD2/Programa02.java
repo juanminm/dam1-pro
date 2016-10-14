01ba3ab8a72ba430d16f3bc19d2fc61b59be7711
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-14 14:59:43 +0200

    Add Programa02 (Ejercicio01UD2)

---
+++ src/dam/programacion/juanminm/Ejercicio01UD2/Programa02.java
@@ -0,0 +1,19 @@
+/*
+ * Ejercicio 2.
+ * 
+ * Escribe un programa que visualice en la pantalla tu nombre completo en 
+ * una línea y en la línea siguiente tu fecha de nacimiento. Para ello escribe 
+ * una sentencia para escribir el nombre, otra para escribir tu primer apellido 
+ * y otra para escribir tu segundo apellido.
+ */
+
+
+
+public class EjercicioU2_02 {
+    public static void main(String[] args) {
+        System.out.print("Juan Miguel ");
+        System.out.print("Navarro ");
+        System.out.println("Martínez");
+        System.out.println("1993/07/16");
+    }
+}
