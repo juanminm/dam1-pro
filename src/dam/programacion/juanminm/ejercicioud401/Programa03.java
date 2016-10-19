2a7b226c694bb50feda2c20480d838e64e6634d5
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-19 09:06:46 +0200

    Add Programa03 (ejercicioud401)

---
+++ src/dam/programacion/juanminm/ejercicioud401/Programa03.java
@@ -0,0 +1,28 @@
+/*
+ * 3. Escribe un método que devuelva el mayor de dos números
+ */
+package dam.programacion.juanminm.ejercicioud401;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+public class Programa03 {
+    static int obtenerMayor (int a, int b) {
+        return a >= b ? a : b;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b;
+
+        System.out.print("Introduzca el primer número a comparar: ");
+        a = scan.nextInt();
+        System.out.print("Introduzca el segundo número a comparar: ");
+        b = scan.nextInt();
+
+        System.out.println("El número mayor es: " + obtenerMayor(a, b));
+    }
+}
