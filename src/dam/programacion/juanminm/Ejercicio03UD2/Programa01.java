b9ac0164eec95b714ed1579c4d229b0285822089
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-10-19 08:18:13 +0200

    Add Programa01 (Ejercicio03UD2)

---
+++ src/dam/programacion/juanminm/Ejercicio03UD2/Programa01.java
@@ -0,0 +1,29 @@
+/*
+ * 1.- Escribe un método que devuelva la suma de dos enteros.
+ */
+package dam.programacion.juanminm.Ejercicio03UD2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+public class Programa01 {
+    static int suma(int operando1, int operando2) {
+        return operando1 + operando2;        
+    }
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int operando1, operando2;
+        
+        System.out.println("Introduzca el primer operando a sumar: ");
+        operando1 = scan.nextInt();
+        System.out.println("Introduzca el segundo operando a sumar: ");
+        operando2 = scan.nextInt();
+        
+        System.out.println("La suma de ambos números es: " + suma(operando1,
+                operando2));
+    }
+}
