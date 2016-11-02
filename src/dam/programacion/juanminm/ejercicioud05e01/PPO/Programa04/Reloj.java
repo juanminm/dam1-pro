bd7ffe054241afdc820c1429e41c25b2de95055b
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-02 09:14:36 +0100

    Add missing exercise comment

--- src/dam/programacion/juanminm/ejercicioud05e01/PPO/Programa04/Reloj.java
+++ src/dam/programacion/juanminm/ejercicioud05e01/PPO/Programa04/Reloj.java
@@ -1,3 +1,27 @@
+/*
+ * 4. Escribe una clase que represente un reloj que señale la hora, el minuto y
+ *    el segundo.
+ *
+ *   a) La clase dispondrá de dos constructores, uno sin parámetros que pone el
+ *      reloj a 0:0:0 y otro al que se le pasa la hora, los minutos y los
+ *      segundos
+ *
+ * Se proporcionarán los siguientes métodos:
+ *
+ *   b) Uno que da la hora, los minutos y los segundos, separados por el
+ *      carácter “:”, en una cadena
+ *   c) Otro que también da la hora pero en formato 24 horas (como el anterior)
+ *      o en formato 12, en cuyo caso debe distinguir entre “am” (para las
+ *      horas de 0 a 11) o “pm” (para las horas de 12 a 23), también en una
+ *      cadena
+ *   d) Un método para poner el reloj en hora. Se le pasa la hora y los
+ *      minutos, poniendo los segundos a 0
+ *   e) Un método para poner el reloj en hora al que, a demás, se le pasan los
+ *      segundos
+ *   f) Añada al ejercicio anterior un método para poner la hora especificando
+ *      si es AM o es PM
+ */
+
 package dam.programacion.juanminm.ejercicioud05e01.PPO.Programa04;
 
 /**
