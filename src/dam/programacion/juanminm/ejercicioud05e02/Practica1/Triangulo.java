b15cce9b28b70542c96e79bc737acf6983df186b
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-04 13:58:37 +0100

    Add attributes, constructors and methods
    Add them to the class Triangulo

--- src/dam/programacion/juanminm/ejercicioud05e02/Practica1/Triangulo.java
+++ src/dam/programacion/juanminm/ejercicioud05e02/Practica1/Triangulo.java
@@ -22,5 +22,23 @@ package dam.programacion.juanminm.ejercicioud05e02.Practica1;
  * @author Juan Miguel Navarro Martínez
  */
 public class Triangulo {
+    private double catetoBase;
+    private double catetoAltura;
+    private double hipotenusa;
 
+    public Triangulo() {
+        catetoBase = 1;
+        catetoAltura = 1;
+        hipotenusa = calcularHipotenusa(catetoBase, catetoAltura);
+    }
+
+    public Triangulo(Punto puntoA, Punto puntoB, Punto puntoC) {
+        catetoBase = puntoA.calcularDistanciaDesde(puntoB);
+        catetoAltura = puntoA.calcularDistanciaDesde(puntoC);
+        hipotenusa = puntoB.calcularDistanciaDesde(puntoC);
+    }
+
+    private double calcularHipotenusa(double catetoBase, double catetoAltura) {
+        return Math.sqrt(Math.pow(catetoBase, 2) + Math.pow(catetoAltura, 2));
+    }
 }
