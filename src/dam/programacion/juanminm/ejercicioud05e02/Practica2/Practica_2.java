0790d4d3288be3e587f59c35930f4cde706d74c7
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-08 10:32:00 +0100

    Finish Practica_2 class

--- src/dam/programacion/juanminm/ejercicioud05e02/Practica2/Practica_2.java
+++ src/dam/programacion/juanminm/ejercicioud05e02/Practica2/Practica_2.java
@@ -34,5 +34,15 @@ package dam.programacion.juanminm.ejercicioud05e02.Practica2;
  * @author Juan Miguel Navarro Martínez
  */
 public class Practica_2 {
+    public static void main(String[] args) {
+        Asignatura asignatura1 = new Asignatura(1),
+                   asignatura2 = new Asignatura(2),
+                   asignatura3 = new Asignatura(3);
+        Alumno alumno = new Alumno(asignatura1, asignatura2, asignatura3);
+        Profesor profesor = new Profesor();
 
+        profesor.ponerNotas(alumno);
+        System.out.printf("Nota media del alumno: %4.2f%n",
+                profesor.calcularMedia(alumno));
+    }
 }
