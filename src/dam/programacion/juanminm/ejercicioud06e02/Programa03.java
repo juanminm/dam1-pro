32aaad6cf3b95b6c207efb2bd936c9a4d8e25a7a
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-21 09:56:04 +0100

    Finish Programa03

--- src/dam/programacion/juanminm/ejercicioud06e02/Programa03.java
+++ src/dam/programacion/juanminm/ejercicioud06e02/Programa03.java
@@ -4,10 +4,29 @@
  */
 package dam.programacion.juanminm.ejercicioud06e02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Juan Miguel Navarro Martínez
  */
 public class Programa03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arrayNum = new int[20];
 
+        for (int i = 1; i <= 20; i++) {
+            System.out.printf("Introduzca el %dº número: ", i);
+            arrayNum[i-1] = scan.nextInt();
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayNum.length; i++) {
+            System.out.printf("%d ", arrayNum[i]);
+
+            if (i % 4 == 3) {
+                System.out.println();
+            }
+        }
+    }
 }
