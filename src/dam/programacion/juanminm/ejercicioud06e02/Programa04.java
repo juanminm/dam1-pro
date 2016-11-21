d6b01808a4643b5603a8588a6432557c050060d1
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-21 10:09:18 +0100

    Finish Programa04

--- src/dam/programacion/juanminm/ejercicioud06e02/Programa04.java
+++ src/dam/programacion/juanminm/ejercicioud06e02/Programa04.java
@@ -6,10 +6,34 @@
  */
 package dam.programacion.juanminm.ejercicioud06e02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Juan Miguel Navarro Martínez
  */
 public class Programa04 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] arrayNum = new int[10];
 
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("Introduzca el %dº número: ", i);
+            arrayNum[i-1] = scan.nextInt();
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayNum.length; i += 1) {
+            if (arrayNum[i] % 2 == 0) {
+                System.out.printf("Pos %d: %d%n", i, arrayNum[i]);
+            }
+        }
+        System.out.println();
+        
+        for (int i = 0; i < arrayNum.length; i += 1) {
+            if (arrayNum[i] % 2 == 1) {
+                System.out.printf("Pos %d: %d%n", i, arrayNum[i]);
+            }
+        }
+    }
 }
