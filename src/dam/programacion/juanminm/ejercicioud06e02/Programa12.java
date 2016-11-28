acb512220152dfa8bbc0b3fcce6c5887f2042467
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-28 22:32:53 +0100

    Finish Programa12

--- src/dam/programacion/juanminm/ejercicioud06e02/Programa12.java
+++ src/dam/programacion/juanminm/ejercicioud06e02/Programa12.java
@@ -14,17 +14,30 @@ import java.util.Scanner;
 public class Programa12 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] arrayNum = new int[10];
 
-        for (int i = 1; i <= 10; i++) {
+        int[] arrayV = new int[10];
+        int[] arrayP = new int[10];
+
+        for (int i = 0; i < arrayV.length; i++) {
             System.out.printf("Introduzca el %dº número: ", i);
-            arrayNum[i-1] = scan.nextInt();
+            arrayV[i] = scan.nextInt();
         }
         System.out.println();
 
-        for (int num: arrayNum) {
+        for (int i = 0; i < arrayP.length; i++) {
+            int j = 9 - i;
+            arrayP[i] = arrayV[j];
+        }
+
+        System.out.printf("Datos del vector V:%n");
+        for (int num: arrayV) {
             System.out.printf("%d ", num);
         }
-        System.out.println();
+
+        System.out.printf("%n%nDatos del vector P:%n");
+        for (int num: arrayP) {
+            System.out.printf("%d ", num);
+        }
+        System.out.printf("%n");
     }
 }
