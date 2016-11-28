1f2b572b990f45c2572150ce6d81b2ac753c1195
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-28 22:56:27 +0100

    Finish Programa14

--- src/dam/programacion/juanminm/ejercicioud06e02/Programa14.java
+++ src/dam/programacion/juanminm/ejercicioud06e02/Programa14.java
@@ -5,10 +5,52 @@
  */
 package dam.programacion.juanminm.ejercicioud06e02;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Juan Miguel Navarro Martínez
  */
 public class Programa14 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        boolean esPar;
+        int posArrayV = 0;
 
+        int[] arrayV = new int[50];
+        int[] arrayP = new int[50];
+
+        for (int i = 0; i < arrayV.length; i++) {
+//            System.out.printf("Introduzca el %dº número: ", i);
+//            arrayV[i-1] = scan.nextInt();
+            arrayV[i] = random.nextInt(50)+1;
+        }
+        System.out.println();
+
+       for (int num: arrayV) {
+            System.out.printf("%d ", num);
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayP.length; i++) {
+            esPar = false;
+            if (posArrayV < arrayV.length) {
+                for (int j = posArrayV; j < arrayV.length; j++) {
+                    if (!esPar) {
+                        if (arrayV[j] % 2 == 0) {
+                            arrayP[i] = arrayV[j];
+                            esPar = true;
+                        }
+                        posArrayV = j + 1;
+                    }
+                }
+            }
+        }
+
+        for (int num: arrayP) {
+            System.out.printf("%d ", num);
+        }
+    }
 }
