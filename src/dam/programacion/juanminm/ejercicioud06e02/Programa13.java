a23584a0616a5d5c78c363a43f3fece2d00b8840
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-11-28 22:43:11 +0100

    Finish Programa13

--- src/dam/programacion/juanminm/ejercicioud06e02/Programa13.java
+++ src/dam/programacion/juanminm/ejercicioud06e02/Programa13.java
@@ -10,10 +10,51 @@
  */
 package dam.programacion.juanminm.ejercicioud06e02;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Juan Miguel Navarro Martínez
  */
 public class Programa13 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
 
+        int[] arrayV = new int[50];
+        int[] arrayP = new int[50];
+
+        for (int i = 0; i < arrayV.length; i++) {
+//            System.out.printf("Introduzca el %dº número: ", i);
+//            arrayV[i-1] = scan.nextInt();
+            arrayV[i] = random.nextInt(50)+1;
+        }
+        System.out.println();
+
+        for (int i = 0; i < arrayP.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                arrayP[i] += arrayV[j];
+            }
+        }
+
+        System.out.printf("Datos del vector V:%n");
+        for (int i = 0; i < arrayV.length; i++) {
+            System.out.printf("%4d ", arrayV[i]);
+
+            if (i % 10 == 9) {
+                System.out.printf("%n");
+            }
+        }
+
+        System.out.printf("%n%nDatos del vector P:%n");
+        for (int i = 0; i < arrayP.length; i++) {
+            System.out.printf("%4d ", arrayP[i]);
+
+            if (i % 10 == 9) {
+                System.out.printf("%n");
+            }
+        }
+        System.out.printf("%n");
+    }
 }
