c97ede4020622dadba8fc04fc40c832b6dd867d8
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2016-12-02 12:25:52 +0100

    Finish Programa03

--- src/dam/programacion/juanminm/ejercicioud06e03/Programa03.java
+++ src/dam/programacion/juanminm/ejercicioud06e03/Programa03.java
@@ -4,10 +4,51 @@
  */
 package dam.programacion.juanminm.ejercicioud06e03;
 
+import java.util.Random;
+
 /**
  *
  * @author Juan Miguel Navarro Martínez
  */
 public class Programa03 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] vectorV = new int[50];
+        int[] vectorP = new int[20];
+        int[][] vectorM = new int[50][20];
 
+        for(int i = 0; i < vectorV.length; i++) {
+            vectorV[i] = random.nextInt(50)+1;
+        }
+
+        for(int i = 0; i < vectorP.length; i++) {
+            vectorP[i] = random.nextInt(50)+1;
+        }
+
+        for (int i = 0; i < vectorM.length; i++) {
+            for (int j = 0; j < vectorM[i].length; j++) {
+                vectorM[i][j] = vectorV[i] * vectorP[j];
+            }
+        }
+
+        System.out.printf("Vector V:%n");
+        for (int num: vectorV) {
+            System.out.printf("%d ", num);
+        }
+        System.out.printf("%n");
+
+        System.out.printf("Vector P:%n");
+        for (int num: vectorP) {
+            System.out.printf("%d ", num);
+        }
+        System.out.printf("%n");
+
+        System.out.printf("Vector M:%n");
+        for (int[] fila: vectorM) {
+            for (int columna: fila) {
+                System.out.printf("%4d ", columna);
+            }
+            System.out.printf("%n");
+        }
+    }
 }
