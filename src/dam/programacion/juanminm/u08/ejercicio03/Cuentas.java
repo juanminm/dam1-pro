9b74688f493ee298b9f9ea794bc32e4de4f34eae
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-01-23 09:04:55 +0100

    Added Cuentas class

---
+++ src/dam/programacion/juanminm/u08/ejercicio03/Cuentas.java
@@ -0,0 +1,12 @@
+package dam.programacion.juanminm.u08.ejercicio03;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+public class Cuentas {
+    private ArrayList<Ccuenta> listaCuentas = new ArrayList();
+    private int total;
+}
