1a33bbc7eb1d8914bfbfda683dc13923300bc30d
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-02-03 11:47:28 +0100

    Add Disco class

---
+++ src/dam/programacion/juanminm/u09/ejercicio01/programa03/Disco.java
@@ -0,0 +1,30 @@
+package dam.programacion.juanminm.u09.ejercicio01.programa03;
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+public class Disco extends Multimedia {
+    private String genero;
+
+    public Disco(String titulo, String autor, int duracion,
+            Formato formato) {
+        super(titulo, autor, duracion, formato);
+    }
+
+    public Disco(String titulo, String autor, int duracion, Formato formato,
+            String genero) {
+        super(titulo, autor, duracion, formato);
+        this.genero = genero;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    @Override
+    public String toString() {
+        String s = super.toString() + "Genero: " + genero + "\n";
+        return s;
+    }
+}
