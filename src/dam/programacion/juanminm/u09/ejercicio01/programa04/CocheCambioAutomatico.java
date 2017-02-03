aee52548bb571f5e2dcf97c9e0ef82006652879b
Juan Miguel Navarro Martínez <juanmi_3000@hotmail.com>
2017-02-03 12:40:13 +0100

    Add CocheCambioAutomatico

---
+++ src/dam/programacion/juanminm/u09/ejercicio01/programa04/CocheCambioAutomatico.java
@@ -0,0 +1,28 @@
+package dam.programacion.juanminm.u09.ejercicio01.programa04;
+
+/**
+ *
+ * @author Juan Miguel Navarro Martínez
+ */
+public class CocheCambioAutomatico extends Coche {
+    public CocheCambioAutomatico(String matricula) {
+        super(matricula);
+    }
+
+    @Override
+    public void acelerar(double aumento) {
+        super.acelerar(aumento);
+        cambiaMarcha(getMarcha() + 1);
+    }
+
+    @Override
+    public void frenar(double decremento) {
+        super.frenar(decremento);
+
+        if (getVelocidad() == 0) {
+            cambiaMarcha(0);
+        } else if (getMarcha() > 1) {
+            cambiaMarcha(getMarcha() - 1);
+        }
+    }
+}
